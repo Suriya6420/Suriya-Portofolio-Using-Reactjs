34ec13ff07cccc2c2bc9e407a3c93755fafdebf3
Suriya6420 <123867140+Suriya6420@users.noreply.github.com>
2025-02-10 22:46:31 -0800

    Update Info.jsx

--- components/about/Info.jsx
+++ components/about/Info.jsx
@@ -15,7 +15,7 @@ const Info = () => {
 				<i className='bx bx-briefcase-alt about__icon'></i>
 
 				<h3 className='about__title'>Project Completion</h3>
-				<span className='about__subtitle'>4</span>
+				<span className='about__subtitle'>3</span>
 			</div>
 
 			<div className='about__box'>
